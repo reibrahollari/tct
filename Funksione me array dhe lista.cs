2cb21d4fe6988fb34a9300b10109b791915c63d1
reibrahollari <reis.brahollari@gmail.com>
2023-02-14 19:48:26 +0100

    Shtuam disa ushtrime

---
+++ Funksione me array dhe lista.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Funksione_me_array_dhe_lista
+    {
+        public int GjejMeTeMadhin(int[] array) {
+
+            int max = 0;
+
+            for (int i = 0; i < array Length; int++)
+            }
+        if(Array[i] > max) max = Array[i];
+            {
+            
+        }
+    
+    
+    public int GJejShumenEArray(int[] array) { }
+        int sum = 0;
+
+        for (int i = 0; i< Array Length; i++) 
+            {
+
+            sum = sum + Array[i];
+
